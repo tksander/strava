a226b0a40d5a0a87d6607049d6aa75f369fadc62
Hack Reactor Students <accounts@hackreactor.com>
2015-09-17 22:16:06 -0700

    Working on request to API

--- server-config.js
+++ server-config.js
@@ -1,8 +1,15 @@
-// Libraries
-
 var express = require('express');
+var handler = require('./lib/request-handler.js');
+
 
 var app = express();
 
+var string = 'https://www.strava.com/api/v3/athlete/activities -H "Authorization: Bearer 491eafe37f405331ffc91eef2d04209cbd74f9f5"'
+
 // Setup routes for API
-module.exports = app;
+app.get('/', function(req, res) {
+  res.send("running get");
+  handler.getRequest(string);
+});
+
+module.exports = app
